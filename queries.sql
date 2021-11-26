0ff1bf15b6aaa3f7cbb790a0cddf08f59233bfa5
Веренич Павел Иванович <p.verenich@ziko.loc>
2021-11-26 15:52:01 +0300

    SQLqueries

---
+++ queries.sql
@@ -0,0 +1,33 @@
+select column1, column2, column3
+from table1;
+
+SELECT * FROM table1;
+
+select CustomrtName, City from table1;
+
+select distinct
+
+select distinct column1, column2
+From table1
+
+select Country from table1;
+
+SELECT COUNT (DISTINCT COUNTRY) FROM TABLE1;
+
+SELECT COUNT(COUNTRIES) FROM TABLE1;
+
+SELECT * FROM TABLE1
+WHERE COUNTRY = 'MEXICO';
+
+select * from table1
+where country= 'germany' and city='berlin';
+
+
+select * from table1
+where country='germany' or country='spain';
+
+select * from table1
+order by city desc;
+
+select * from table1
+order by country asc, name desc;
